044d93b7866eb8cb8e45d2872bcad7f9ced12dea
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-08-20 15:11:12 +0530

    feat: Custom Fonts!

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import styled from "styled-components/native";
+
+import {
+  useFonts as useInterFont,
+  Inter_500Medium,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter";
+
+import {
+  useFonts as usePoppinsFont,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
 
 const Title = styled.Text`
   padding: 16px;
@@ -9,6 +22,21 @@ const Title = styled.Text`
 `;
 
 export default function App() {
+  const [interLoaded] = useInterFont({
+    Inter_500Medium,
+    Inter_600SemiBold,
+  });
+
+  const [poppinsLoaded] = usePoppinsFont({
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+  });
+
+  if (!interLoaded || !poppinsLoaded) {
+    return;
+  }
+
   return (
     <View style={styles.container}>
       <Title>Hello</Title>
