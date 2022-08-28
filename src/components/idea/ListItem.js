51abe7449564031d3afb9b9ce33e42186c40dabb
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-08-28 20:21:35 +0530

    fix: Truncate idea title

--- src/components/idea/ListItem.js
+++ src/components/idea/ListItem.js
@@ -6,6 +6,8 @@ import styled from "styled-components";
 import { useNavigation } from "@react-navigation/native";
 import { CardSubtitle, CardTitle, CardContainer } from "../utils/Card";
 
+const MAX_TITLE_SHOW_LENGTH = 14;
+
 const ItemContainer = styled(CardContainer)`
   width: 100%;
   padding: 18px 14px;
@@ -22,13 +24,19 @@ const Emoji = styled.Text`
 export const IdeaListItem = ({ ideaData }) => {
   const navigation = useNavigation();
   const { emoji, title, createdAt, id: ideaId } = ideaData;
+
+  // Truncate the title so it fits in the idea list item card
+  const truncatedTitle =
+    title.slice(0, MAX_TITLE_SHOW_LENGTH) +
+    (title.length > MAX_TITLE_SHOW_LENGTH ? "..." : "");
+
   return (
     <Pressable onPress={() => navigation.push("IdeaDetail", { ideaId })}>
       <ItemContainer>
         <Row>
           <Emoji>{emoji}</Emoji>
           <View style={{ marginLeft: 10 }}></View>
-          <CardTitle>{title}</CardTitle>
+          <CardTitle>{truncatedTitle}</CardTitle>
         </Row>
 
         <CenteredRow>
