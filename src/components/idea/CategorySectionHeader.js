68dec487c8f7bffd0de49281b84a94f310286758
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-09-03 00:54:18 +0530

    refactor: Separate out category section header component

---
+++ src/components/idea/CategorySectionHeader.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View } from "react-native";
+import styled from "styled-components";
+
+import { CardSubtitle } from "../../components/utils/Card";
+import { CenteredRow } from "../../components/utils/Row";
+const CategoryHeaderEmoji = styled.Text`
+  font-size: ${({ theme }) => theme.fontSizes.xl};
+`;
+
+const CategoryHeaderTitle = styled.Text`
+  font-size: ${({ theme }) => theme.fontSizes.lg};
+  font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+  font-family: ${({ theme }) => theme.fonts.secondaryHeading};
+`;
+
+const CategoryHeaderContainer = styled(CenteredRow)`
+  justify-content: space-between;
+  align-content: center;
+  margin-bottom: 18px;
+`;
+
+export const CategorySectionHeader = ({
+  section: { title, emoji, noOfIdeas },
+}) => {
+  return (
+    <CategoryHeaderContainer>
+      <CenteredRow>
+        <CategoryHeaderEmoji>{emoji}</CategoryHeaderEmoji>
+        <View style={{ marginLeft: 15 }}></View>
+        <CategoryHeaderTitle>{title}</CategoryHeaderTitle>
+      </CenteredRow>
+
+      <CardSubtitle>
+        {noOfIdeas} {noOfIdeas === 1 ? "Idea" : "Ideas"}
+      </CardSubtitle>
+    </CategoryHeaderContainer>
+  );
+};
--- src/screens/idea/FullListScreen.js
+++ src/screens/idea/FullListScreen.js
@@ -1,48 +1,16 @@
 import React, { useContext } from "react";
-import { View, SectionList } from "react-native";
+import { SectionList } from "react-native";
 import styled from "styled-components";
-import { getProcessedCategoriesList } from "../../components/idea/CategorySelect";
 import { IdeaListItem } from "../../components/idea/ListItem";
-import { CardSubtitle } from "../../components/utils/Card";
-import { CenteredRow } from "../../components/utils/Row";
+import { getProcessedCategoriesList } from "../../components/idea/CategorySelect";
+import { CategorySectionHeader } from "../../components/idea/CategorySectionHeader";
+
 import { IdeaContext } from "../../provider/idea";
 
 const PageFrame = styled.View`
   margin: 10px 24px;
   flex: 1;
 `;
-
-const CategoryHeaderEmoji = styled.Text`
-  font-size: ${({ theme }) => theme.fontSizes.xl};
-`;
-
-const CategoryHeaderTitle = styled.Text`
-  font-size: ${({ theme }) => theme.fontSizes.lg};
-  font-weight: ${({ theme }) => theme.fontWeights.semiBold};
-  font-family: ${({ theme }) => theme.fonts.secondaryHeading};
-`;
-
-const CategoryHeaderContainer = styled(CenteredRow)`
-  justify-content: space-between;
-  align-content: center;
-  margin-bottom: 18px;
-`;
-
-const CategorySectionHeader = ({ section: { title, emoji, noOfIdeas } }) => {
-  return (
-    <CategoryHeaderContainer>
-      <CenteredRow>
-        <CategoryHeaderEmoji>{emoji}</CategoryHeaderEmoji>
-        <View style={{ marginLeft: 15 }}></View>
-        <CategoryHeaderTitle>{title}</CategoryHeaderTitle>
-      </CenteredRow>
-
-      <CardSubtitle>
-        {noOfIdeas} {noOfIdeas === 1 ? "Idea" : "Ideas"}
-      </CardSubtitle>
-    </CategoryHeaderContainer>
-  );
-};
 
 const CategorySectionDivider = styled.View`
   margin-bottom: 52px;
